f8444ce9162d8b05825cd462d4399e80963cbe7f
Carolina Andrade <carolina.andrade@sensedia.com>
2023-04-14 10:44:47 -0300

    Project created

---
+++ src/main/java/com/educandoweb/course1nelio/Course1NelioApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course1nelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course1NelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course1NelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/course1nelio/Course1NelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course1nelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course1NelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
